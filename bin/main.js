85a85e4d41efec6d011d4d2e991ffc95c6a673e4
Adam <technomada@users.noreply.github.com>
2019-10-31 21:20:31 -0400

    remove init

--- bin/main.js
+++ bin/main.js
@@ -10,40 +10,11 @@ const webpack = require('webpack');
 //
 
 program
-  .command('init [template]')
-  .description('setup local environment')
-  //.option("-s, --setup_mode [mode]", "Which setup mode to use")
-  .action(function(env, options){
-    //var mode = options.setup_mode || "normal";
-    env = env || 'all';
-    //console.log('setup for %s env(s) with %s mode', env, mode);
-  	//console.log({env,options})
-
-	// upg: warn if existing build.
-	// upg: unzip package file? run init.js from it?
-	//fs.mkdirSync('app')
-	//fs.writeFileSync('app/test.txt','hello.')
-	 
-	let from = __dirname+'/../templates/default'
-	let to = './app'
-	if(!fs.existsSync(to)){
-		fs.copy(from+'/files',to).then(v=>{}) // upg: error control?
-		let init = require(from+'/init.js')
-		if(typeof(init) == 'function'){
-			init({to:path.resolve(to)}) // upg pass {opts}
-			}
-		}//if
-	else
-	  	console.log(to,'exists')
-  	});
-
-program
 	.command('build')
 	.description('build application')
 	.action(function(env,opts){
-		console.log("BUILD!")
-
-		//const base = path.resolve('.')+'/app'
+		console.log("BUILDING...")
+		
 		const base = path.resolve('.')
 		const src = base+'/src'
 		const dist = base+'/dist'
